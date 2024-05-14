7bb06cc6de02e341af6b604c1f42fec496f7a1c7
llswll023 <llswll023@gmail.com>
2024-05-15 01:14:32 +0900

    Project basic settings

---
+++ src/main/java/com/example/swproj22/Swproj22Application.java
@@ -0,0 +1,13 @@
+package com.example.swproj22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swproj22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swproj22Application.class, args);
+	}
+
+}
